cef3d831ec49936f749acfb785a7dbc57af43fd0
Marce Graffigna <marcesmartphone@gmail.com>
2019-07-24 20:45:09 +0100

    first commit

---
+++ proyectoAhorcado.js
@@ -0,0 +1,28 @@
+//array inicial de peliculas ej:
+var array = [{
+    nombre: "Los Vengadores",
+    pistas: ["Son un equipo", ""]
+}, {
+    nombre: "Iron-Man",
+    pistas: ["asdasdasd", "asdasdasd"]
+},
+{
+    nombre: "Sully",
+    pistas: ["asdasdasd", "asdasdasd"]
+},
+{
+    nombre: "DeadPool",
+    pistas: ["asdasdasd", "asdasdasd"]
+},
+{
+    nombre: "Terminal",
+    pistas: ["asdasdasd", "asdasdasd"]
+},
+{
+    nombre: "El Rey León",
+    pistas: ["asdasdasd", "asdasdasd"]
+},
+]
+
+//filter de pelicula respecto a la letra introducida
+
