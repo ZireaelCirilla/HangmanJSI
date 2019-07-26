817571cd760235b7ee6cc943be27951d6cac5231
nestorhdez <nestorhdez.26@gmail.com>
2019-07-26 16:19:42 +0100

    Tied functions to print characters that match.

--- js/hangman.js
+++ js/hangman.js
@@ -2,7 +2,7 @@ var guessArr = [{
     name: "Los Vengadores",
     clues: ["Son un equipo", "Protejen la tierra"]
 }, {
-    name: "Iron-Man",
+    name: "Iron Man",
     clues: ["Es rojo", "Parece hecho de oro"]
 },
 {
@@ -18,10 +18,64 @@ var guessArr = [{
     clues: ["Deseo de un padre", "Hijo ejemplar"]
 },
 {
-    name: "El Rey León",
+    name: "El Rey Leon",
     clues: ["Live Action", "Hakuna-Matata"]
 },
 ];
+
+const htmlButton = (val) => {
+    return `<button id="btn-${val}" class="letter-btn">${val}</button>`;
+}
+
+const renderButtons = () => {
+    let abc = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'.split('');
+    return abc.map(letter => htmlButton(letter)).join('');
+}
+
+const addListenerButtons = (word, callback) => {
+    let arrBtn = document.querySelectorAll('.letter-btn');
+    arrBtn.forEach(btn => btn.addEventListener('click', (e) => {callback(word, e)}));
+}
+
+const htmlLifeIcon = () => {
+    return '<div class="life"><i class="fas fa-heart"></i></div>';
+}
+
+const renderLifeIcons = (NumOfLifes) => {
+    let lifes = [];
+    for (let i = 0; i < NumOfLifes; i++) {
+        lifes.push(htmlLifeIcon());
+    }
+    return lifes.join('');
+}
+
+const htmlLetterContainer = (letter) => {
+    let space = '<div class="letter-container"></div>';
+    let htmlLetter = (`
+        <div class="letter-container">
+            <div class="letter"></div>
+            <div class="underline"></div>
+        </div>
+    `);
+
+    return letter !== ' ' ? htmlLetter : space;
+}
+
+const renderLetterContainers = (titleFilm) => {
+    let titleArr = titleFilm.split('');
+    return titleArr.map(val => htmlLetterContainer(val)).join('');
+}
+const printChar = (word, e) => {
+    let char = e.target.innerHTML;
+    let letters = document.querySelectorAll('.letter');
+    let charIndexArr = getCharacterMatches(word, char);
+    e.target.setAttribute('disabled', '');
+    if(charIndexArr != -1) {
+        charIndexArr.forEach(index => {
+            letters[index].innerHTML = char;
+        } );
+    }
+}
 
 startGame()
 
@@ -29,6 +83,9 @@ function startGame() {
     var playing = true;
     var wordObj = getGuessingWord(guessArr);
     var wordName = wordObj.name
+    document.querySelector('.keyboard--container').innerHTML = renderButtons();
+    addListenerButtons(wordName, printChar);
+    document.querySelector('.characters--container').innerHTML = renderLetterContainers(wordName);
 
     //Testing for the clue generator
     // var clues1 = generateClue(wordObj)
@@ -47,10 +104,13 @@ function startGame() {
 
 }
 
+
+
 function getCharacterMatches(word, char) {
+    word = word.replace(/\s/g, '');
     var indices = [];
     for (var i = 0; i < word.length; i++) {
-        if (word[i] === char) indices.push(i);
+        if (word[i].toLowerCase() == char.toLowerCase()) indices.push(i);
     }
     if (indices != []) {
         return indices
@@ -84,40 +144,4 @@ function generateClue(wordObj) {
             return "No more clues :("
         }
     }
-}
-
-const htmlButton = (val) => {
-    return `<button id="btn-${val}" class="letter-btn">${val}</button>`;
-}
-
-const renderButtons = () => {
-    let abc = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'.split('');
-    return abc.map(letter => htmlButton(letter)).join('');
-}
-
-const htmlLifeIcon = () => {
-    return '<div class="life"><i class="fas fa-heart"></i></div>';
-}
-
-const renderLifeIcons = (NumOfLifes) => {
-    let lifes = [];
-    for (let i = 0; i < NumOfLifes; i++) {
-        lifes.push(htmlLifeIcon());
-    }
-    return lifes.join('');
-}
-
-const htmlLetterContainer = (letter) => {
-    return (`
-        <div class="letter-container">
-            <div class="letter"></div>
-            <div class="underline"></div>
-        </div>
-    `);
-}
-
-const renderLetterContainers = (titleFilm) => {
-    let space = '<div class="letter-container"></div>';
-    let titleArr = titleFilm.split('');
-    return titleArr.map(val => val !== ' ' ? htmlFilmLetter(val) : space).join('');
 }
